7d37c2d3dadb4a01ddc847925cdb338c9e40076c
Miguel Ángel Hernández <miguelangel.hermar410@gmail.com>
2023-09-28 23:33:27 +0000

    sql_query added in models directory

---
+++ models/new_query.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized="table"
+    )
+}}
+
+
+
+SELECT 
+provider_id as inpatient_provider_id, 
+provider_name as inpatient_provider_name,
+provider_city as inpatient_provider_city,
+provider_state as inpatient_provider_state,
+ROUND(SUM(total_discharges), 2) as inpatient_sum_total_discharges,
+ROUND(SUM(average_covered_charges), 2) as inpatient_sum_average_covered_discharges,
+ROUND(SUM(average_total_payments), 2) as inpatient_sum_average_total_payments,
+ROUND(SUM(average_medicare_payments), 2) as inpatient_sum_medicare_total_payments,
+FROM `bigquery-public-data.cms_medicare.inpatient_charges_2011`
+GROUP BY 
+1, 2, 3, 4
